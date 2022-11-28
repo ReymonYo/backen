009e0c2b208c7619cabedfd7c664900861410d43
ReymonYo <Reymon_99@outlook.com>
2022-11-28 10:35:03 -0600

    Mi primer servidor de express

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.listen(port, () => {
+console.log(`servidor listo y escuchando en el puerto: ${port}`)
+
+})
