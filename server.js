786233772b2b2f9e413e20417ce5a7a9accf7ded
ReymonYo <Reymon_99@outlook.com>
2022-11-28 10:43:38 -0600

    Crear ruta nueva y usar metodo get

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ app.use(express.json());
 app.listen(port, () => {
 console.log(`servidor listo y escuchando en el puerto: ${port}`)
 
+});
+app.get("/", function(request, response){
+response.send("backend funcionando")
 })
